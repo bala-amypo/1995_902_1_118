b067a322a02fa49a52c8e2c9bf1e2d204940c7f3
bala-amypo <bala@amypo.in>
2025-12-17 05:17:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/student.java
+++ src/main/java/com/example/demo/entity/student.java
@@ -1,28 +1,32 @@
 package com.example.project.entity;
 import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.per
+
 public class  student{
     @Id
-    
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String email;
-    private LocalDate dob;
-}
-public int getId(){
-    return id;
-}
-public void setId(int id){
-    this.id=id;
-}
-public String getName(){
-    return name;
-}
-public void setName(String name){
-    this.name=name;
-}
-public String getEmail(){
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
+    public int getId(){
+        return id;
+        }
+        public void setId(int id){
+            this.id=id;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getEmail(){
+            return email;
+        }
+        public void setEmail(String email){
+            this.email=email;
 }
