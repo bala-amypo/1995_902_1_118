565c72902d3c5d142f19df38379131c1453ce112
bala-amypo <bala@amypo.in>
2025-12-20 16:58:11 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/DelayScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/DelayScoreServiceImpl.java
@@ -1,4 +1,42 @@
-pacakage com.example.demo.service.Impl;
-public class DelayScoreServiceImpl implements DelayScoreService{
-    
+package com.example.demo.service;
+
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
+import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class DelayScoreServiceImpl implements DelayScoreService {
+
+    private final PurchaseOrderRecordRepository poRepo;
+    private final DeliveryRecordRepository deliveryRepo;
+    private final DelayScoreRecordRepository delayRepo;
+
+    public DelayScoreServiceImpl(PurchaseOrderRecordRepository poRepo,
+                                 DeliveryRecordRepository deliveryRepo,
+                                 DelayScoreRecordRepository delayRepo) {
+        this.poRepo = poRepo;
+        this.deliveryRepo = deliveryRepo;
+        this.delayRepo = delayRepo;
+    }
+
+    @Override
+    public DelayScoreRecord compute(Long poId) {
+
+        PurchaseOrderRecord po = poRepo.findById(poId).orElseThrow();
+        DeliveryRecord d = deliveryRepo.findByPoId(poId).get(0);
+
+        long days = ChronoUnit.DAYS.between(
+                po.getPromisedDeliveryDate(),
+                d.getActualDeliveryDate());
+
+        DelayScoreRecord r = new DelayScoreRecord();
+        r.setPoId(poId);
+        r.setSupplierId(po.getSupplierId());
+        r.setDelayDays((int) days);
+        r.setDelaySeverity(days <= 0 ? "ON_TIME" : "DELAYED");
+
+        return delayRepo.save(r);
+    }
 }
