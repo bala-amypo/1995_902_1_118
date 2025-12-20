5fc90e397bdbc2cf6027e1f3748c6b5e6a8ca7fa
bala-amypo <bala@amypo.in>
2025-12-20 18:07:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/DelayScoreRecord.java
+++ src/main/java/com/example/demo/entity/DelayScoreRecord.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "delay_score_record")
 public class DelayScoreRecord {
 
     @Id
@@ -10,15 +11,49 @@ public class DelayScoreRecord {
     private Long id;
 
     private Long poId;
+
+    private Long supplierId;
+
     private int delayDays;
+
     private String delaySeverity;
+
+    // -------- GETTERS & SETTERS --------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPoId() {
+        return poId;
+    }
 
     public void setPoId(Long poId) {
         this.poId = poId;
     }
 
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public int getDelayDays() {
+        return delayDays;
+    }
+
     public void setDelayDays(int delayDays) {
         this.delayDays = delayDays;
+    }
+
+    public String getDelaySeverity() {
+        return delaySeverity;
     }
 
     public void setDelaySeverity(String delaySeverity) {
