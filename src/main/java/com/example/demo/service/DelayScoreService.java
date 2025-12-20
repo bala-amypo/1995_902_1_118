f5a74e4075a3f1812d547f73d87355925724d2cc
bala-amypo <bala@amypo.in>
2025-12-20 17:55:11 +0000

    User commit

--- src/main/java/com/example/demo/service/DelayScoreService.java
+++ src/main/java/com/example/demo/service/DelayScoreService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.DelayScoreRecord;
+import com.example.demo.entity.DelayScoreRecord;
 
 public interface DelayScoreService {
     DelayScoreRecord compute(Long poId);
--- src/main/java/com/example/demo/service/Impl/DelayScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/DelayScoreServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.*;
+import com.example.demo.entity.*;
 import com.example.demo.repository.*;
 import org.springframework.stereotype.Service;
 
