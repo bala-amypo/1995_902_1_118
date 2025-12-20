35d68b00495b973be059f685015ba773ec80bfd6
bala-amypo <bala@amypo.in>
2025-12-20 17:07:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/DelayScoreController.java
+++ src/main/java/com/example/demo/controller/DelayScoreController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
-public class DelayScoreController{
-    
+
+import com.example.demo.model.DelayScoreRecord;
+import com.example.demo.service.DelayScoreService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/delay-scores")
+public class DelayScoreController {
+
+    private final DelayScoreService service;
+
+    public DelayScoreController(DelayScoreService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/compute/{poId}")
+    public DelayScoreRecord compute(@PathVariable Long poId) {
+        return service.compute(poId);
+    }
 }
