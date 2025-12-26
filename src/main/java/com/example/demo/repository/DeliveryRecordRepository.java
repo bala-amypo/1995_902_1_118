4063a970a9fdb83d1a71678ef00e66ef973a5148
bala-amypo <bala@amypo.in>
2025-12-26 16:03:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeliveryRecordRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.DeliveryRecord;
+import com.example.demo.entity.DelayScoreRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface DeliveryRecordRepository
-        extends JpaRepository<DeliveryRecord, Long> {
-
-    DeliveryRecord findByPoId(Long poId);
+public interface DelayScoreRecordRepository extends JpaRepository<DelayScoreRecord, Long> {
+    List<DelayScoreRecord> findBySupplierId(Long supplierId);
 }
--- src/main/java/com/example/demo/repository/PurchaseOrderRecordRepository.java
+++ src/main/java/com/example/demo/repository/PurchaseOrderRecordRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.PurchaseOrderRecord;
+import com.example.demo.entity.SupplierProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface PurchaseOrderRecordRepository
-        extends JpaRepository<PurchaseOrderRecord, Long> {
-    List<PurchaseOrderRecord> findBySupplierId(Long supplierId);
+public interface SupplierProfileRepository
+        extends JpaRepository<SupplierProfile, Long> {
 }
